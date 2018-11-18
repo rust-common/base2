2a7f1ad14aa8608b1beb372d694523dfb7f4b73b
Sergey Shandar <sergey.shandar@gmail.com>
2018-11-17 19:26:30 -0800

    traits based.

--- src/lib.rs
+++ src/lib.rs
@@ -1,49 +1,66 @@
-/// `floor(log(2, v))`
-///
-/// Examples
-///
-/// ```rust
-/// assert_eq!(base2::floor_log2(0), 0);
-/// assert_eq!(base2::floor_log2(1), 0);
-/// assert_eq!(base2::floor_log2(2), 1);
-/// assert_eq!(base2::floor_log2(3), 1);
-/// assert_eq!(base2::floor_log2(4), 2);
-/// assert_eq!(base2::floor_log2(8), 3);
-/// ```
-pub fn floor_log2(mut v: u32) -> u32 {
-    let mut result = 0;
-    loop {
-        v /= 2;
-        if v == 0 { break; }
-        result += 1;
+extern crate int;
+
+use int::UInt;
+
+pub trait Base2 {
+    /// `floor(log(2, v))`
+    ///
+    /// Examples
+    ///
+    /// ```rust
+    /// use base2::Base2;
+    /// assert_eq!(0_u8.floor_log2(), 0);
+    /// assert_eq!(1_u16.floor_log2(), 0);
+    /// assert_eq!(2_u32.floor_log2(), 1);
+    /// assert_eq!(3_u64.floor_log2(), 1);
+    /// assert_eq!(4_u128.floor_log2(), 2);
+    /// assert_eq!(8_usize.floor_log2(), 3);
+    /// ```
+    fn floor_log2(self) -> u8;
+
+    /// `2^p`
+    ///
+    /// Examples
+    ///
+    /// ```rust
+    /// use base2::Base2;
+    /// assert_eq!(u8::exp2(0), 1);
+    /// assert_eq!(u16::exp2(1), 2);
+    /// assert_eq!(u32::exp2(2), 4);
+    /// assert_eq!(u64::exp2(3), 8);
+    /// assert_eq!(u128::exp2(4), 16);
+    /// assert_eq!(usize::exp2(5), 32);
+    /// ```
+    fn exp2(p: u8) -> Self;
+
+    /// A mask with a `p` number of bits.
+    ///
+    /// ```
+    /// use base2::Base2;
+    /// assert_eq!(u8::mask(0), 0);
+    /// assert_eq!(u16::mask(1), 1);
+    /// assert_eq!(u32::mask(2), 0b11);
+    /// assert_eq!(u64::mask(3), 0b111);
+    /// assert_eq!(u128::mask(4), 0b1111);
+    /// assert_eq!(usize::mask(5), 0b11111);
+    /// assert_eq!(u8::mask(8), 0b1111_1111);
+    /// ```
+    fn mask(p: u8) -> Self;
+}
+
+impl<T: UInt> Base2 for T {
+    fn floor_log2(mut self) -> u8 {
+        let mut result = 0;
+        loop {
+            self >>= 1;
+            if self == Self::_0 { break; }
+            result += 1;
+        }
+        result
     }
-    result
+    fn exp2(p: u8) -> Self { Self::_1 << p }
+    fn mask(p: u8) -> Self {
+        if p >= Self::BIT_COUNT { Self::MAX_VALUE } else { Self::exp2(p) - Self::_1 }
+    }
 }
 
-/// `2^p`
-///
-/// Examples
-///
-/// ```rust
-/// assert_eq!(base2::pow2(0), 1);
-/// assert_eq!(base2::pow2(1), 2);
-/// assert_eq!(base2::pow2(2), 4);
-/// assert_eq!(base2::pow2(3), 8);
-/// ```
-pub fn pow2(p: u8) -> u32 {
-    1 << p
-}
-
-/// A mask with a `p` number of bits.
-///
-/// ```
-/// assert_eq!(base2::mask(0), 0);
-/// assert_eq!(base2::mask(1), 1);
-/// assert_eq!(base2::mask(2), 0b11);
-/// assert_eq!(base2::mask(3), 0b111);
-/// assert_eq!(base2::mask(4), 0b1111);
-/// assert_eq!(base2::mask(5), 0b11111);
-/// ```
-pub fn mask(p: u8) -> u32 {
-    pow2(p) - 1
-}
